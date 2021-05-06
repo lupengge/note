d20945351da135d8e3125df35bd06b766152a8b3
lupengge <1739357748@qq.com>
2021-05-06 14:54:48 +0800

    1

--- TEST/testJS/test/server.js
+++ TEST/testJS/test/server.js
@@ -10,6 +10,7 @@ app.use('/', (req, res, next) => {
 	res.status(200).sendFile(path.resolve(__dirname, 'index.html'))
 })
 
+
 io.on('connection', client => {
 	console.log(client.id, '=======================')
 	client.on('channel', data => {

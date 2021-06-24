b92d9a1595a94978938606eff64e64cbeb929b1e
lpg <1739357748@qq.com>
2021-06-24 17:41:18 +0800

    下载网页图片

--- TEST/pythonTest/抠图/imageMatting.py
+++ TEST/pythonTest/抠图/imageMatting.py
@@ -1,14 +1,22 @@
 import os
-import sys
 import paddlehub as hub
 import json
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import math
-from PIL import Image
+from alive_progress import alive_bar
 
-floor = math.floor
+
+def cssColorString2list(string: str) -> list:
+    '''
+      输入一个css颜色字符串返回一个十进制数组
+    '''
+    out = []
+    out.append(int(string[1:3], 16))
+    out.append(int(string[3:5], 16))
+    out.append(int(string[5:], 16))
+    return out
+
 
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -21,41 +29,53 @@ class NpEncoder(json.JSONEncoder):
         else:
             return super(NpEncoder, self).default(obj)
 
+
 # 1.加载模型
 humanSeg = hub.Module(name="deeplabv3p_xception65_humanseg")
 
 # 2.指定待抠图图片目录
-path = 'C:/Users/lpg/Desktop/testPyIMG/'
+path = input('图片所在文件夹：')
+
 files = []
+
 dirs = os.listdir(path)
 for direction in dirs:
- files.append(path + direction)
+    files.append(path +'\\'+direction)
+
+colorStr = input('切换背景色为(cssString):')
+color = cssColorString2list(colorStr)
 
 # 3.抠图
 results = humanSeg.segmentation(data={"image": files})
 
+with alive_bar(len(files)) as bar :
 
-img = mpimg.imread(files[1])
+  for i, file in enumerate(files):
 
-human_range=results[1]['data']
+      img = mpimg.imread(file)
 
-human_range=[[1 if cel>0 else 0 for cel in row]for row in human_range]
+      human_range = results[i]['data']
 
-r=img[:,:,0]*human_range
-g=img[:,:,1]*human_range
-b=img[:,:,2]*human_range
+      human_range = [[1 if cel > 0 else 0 for cel in row]for row in human_range]
+      try:
+        human_range=np.array(human_range).transpose()
+        r = img[:, :, 0]*human_range
+        g = img[:, :, 1]*human_range
+        b = img[:, :, 2]*human_range
+      except ValueError:
+        human_range=np.array(human_range).transpose()
+        r = img[:, :, 0]*human_range
+        g = img[:, :, 1]*human_range
+        b = img[:, :, 2]*human_range
 
-result=[[[r[i,j],g[i,j],b[i,j]] if r[i,j]>0 else [0,222,253] for i in range(r.shape[0])]for j in range(r.shape[1])]
+      result = [[[r[i, j], g[i, j], b[i, j]] if r[i, j] > 0 else color for j in range(r.shape[1])]for i in range(r.shape[0])]
 
-bg=[[[255,0,0] if cel==0 else 0 for cel in row]for row in human_range]
+      # 展示预测结果图片
+      result = np.array(result)
 
-# 展示预测结果图片
-result=np.array(result)
-
-
-
-plt.imshow(result)
-plt.axis('off')
-plt.show()
-res = Image.fromarray(np.uint8(result))
-res.save(path+'123121.png')
+      plt.imshow(result)
+      plt.axis('off')
+      if not os.path.exists(path+'/output/'):
+        os.mkdir(path+'/output/')
+      plt.imsave(path+'/output/'+dirs[i], np.uint8(result))
+      bar()

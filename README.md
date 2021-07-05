8bb03d66b3d53ea35951e42cff1b61a8eaaa3cbe
lpg <1739357748@qq.com>
2021-07-05 15:07:08 +0800

    添加主页目录更新脚本

--- README.md
+++ README.md
@@ -1,33 +1,35 @@
-# myNotes
+# 各种笔记
 
-#### 介绍
+## 所有文档
+<!-- 所有文档 -->
+### Git相关
+[Git用户名错误](/Git相关/Git用户名错误.md)  
+### css相关
+[问题](/css相关/问题.md)  
+### electron相关
+[通信](/js相关/electron相关/通信.md)  
+### js相关
+[API总结](/js相关/API总结.md)  
+[vuex笔记](/js相关/vuex笔记.md)  
+[webWorker](/js相关/webWorker.md)  
+[各种js包](/js相关/各种js包.md)  
+[提纲](/js相关/提纲.md)  
+[监听Dom和属性](/js相关/监听Dom和属性.md)  
+### myNotes
+[README](/README.md)  
+[_404](/_404.md)  
+### unity
+[Csharp知识点](/unity/Csharp知识点.md)  
+[UIToolKit](/unity/UIToolKit.md)  
+[unity学习时积累](/unity/unity学习时积累.md)  
+[包说明](/unity/包说明.md)  
+[插件mirror的使用](/unity/插件mirror的使用.md)  
+[搭建一个目录树](/unity/搭建一个目录树.md)  
+### 往窗口中拖文件
+[README](/unity/往窗口中拖文件/README.md)  
+<!-- 所有文档 -->
 
-* ### unity
-    * [Unity积累](/unity/unity学习时积累)
-    * [UIToolKit](/unity/UI/UIToolKit)
-    * [C#积累](/unity/Csharp知识点)
-    * [一个ui练习](/unity/搭建一个目录树)
-* ### js相关
-    * [js包总结](/js相关/各种js包)
-    * [考试提纲](/js相关/提纲)
-* ### git相关
-    * [git修改用户信息](/Git相关/Git用户名错误)
 
-##### 所有文档
-[问题](/css相关/问题.md)
-[Git用户名错误](/Git相关/Git用户名错误.md)
-[API总结](/js相关/API总结.md)
-[vuex笔记](/js相关/vuex笔记.md)
-[webWorker](/js相关/webWorker.md)
-[各种js包](/js相关/各种js包.md)
-[提纲](/js相关/提纲.md)
-[监听Dom和属性](/js相关/监听Dom和属性.md)
-[README](/README.md)
-[Csharp知识点](/unity/Csharp知识点.md)
-[UIToolKit](/unity/UI/UIToolKit.md)
-[unity学习时积累](/unity/unity学习时积累.md)
-[包说明](/unity/包说明.md)
-[README](/unity/往窗口中拖文件/UnityWindowsFileDrag-Drop-master/README.md)
-[插件mirror的使用](/unity/插件mirror的使用.md)
-[搭建一个目录树](/unity/搭建一个目录树.md)
-[_404](/_404.md)
+### 快速找到本项目中所有的笔记md文件
+
+运行根目录下的getMdPath.js
--- getMdPaths.js
+++ getMdPaths.js
@@ -2,37 +2,69 @@ const fs = require('fs')
 const os = require('os')
 const path = require('path')
 
-
-function getAllMd(parentDir){
-  let result=[]
-  if(fs.readdirSync(parentDir).length==0){
+/**
+ * 
+ * @param {string} parentDir 入口路径
+ * @returns {Array}
+ */
+function getAllMd(parentDir) {
+  let result = []
+  if (fs.readdirSync(parentDir).length == 0) {
     return result;
-  }else{
+  } else {
     childDirs = fs.readdirSync(parentDir)
-    for(let dir of childDirs){
-      if(dir=="node_modules")continue
+    for (let dir of childDirs) {
+      if (dir == "node_modules") continue
 
-      let state=fs.lstatSync(path.join(parentDir,dir))
-      if(state.isDirectory()){
-        
-        result.push(...(getAllMd(path.join(parentDir,dir))))
-      }else if(dir.indexOf(".md")>-1){
+      let state = fs.lstatSync(path.join(parentDir, dir))
+      if (state.isDirectory()) {
+
+        result.push(...(getAllMd(path.join(parentDir, dir))))
+      } else if (dir.indexOf(".md") > -1) {
         //判断路径中中是否含有.md
-        result.push({path:path.join(parentDir,dir),name:dir.substring(0,dir.length-3)})
-        
+        let parentName = path.basename(parentDir, "");
+        result.push({ 
+          path: path.join(parentDir, dir), 
+          name: dir.substring(0, dir.length - 3), 
+          parent: parentName 
+        })
       }
-      
+
     }
     return result;
   }
 }
+
 let out = getAllMd(__dirname)
-let a="";
-for(let i of out){
-  a+=`[${i.name}](${i.path})\n`
+
+
+out = out.sort((a,b)=>{
+  return a.parent[0].charCodeAt()-b.parent[0].charCodeAt()
+})
+
+let a = "\n";
+let lastParentName = "";
+for (let i of out) {
+  let name = i.name;
+  if (lastParentName !== i.parent) {
+    a += `### ${i.parent}\n`
+    lastParentName = i.parent;
+  }
+
+  let path = i.path.substr(__dirname.length).replace(/\\/g, '/')
+  a += `[${name}](${path})  \n`
 }
 
-fs.writeFileSync('./res.txt',a)
+// fs.writeFileSync('./res.txt',a)
+
+let content = fs.readFileSync('./README.md', 'utf-8')
+content = content.split("<!-- 所有文档 -->")
+content[1] = a;
+
+fs.writeFileSync('README.md', content.join('<!-- 所有文档 -->'), { flag: "w+" })
+
+console.log('更新成功')
+
 
 
 
